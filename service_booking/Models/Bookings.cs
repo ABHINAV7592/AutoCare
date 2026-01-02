279c731ee15e39fd3258e548d2f28fa2ea1cb849
ABHINAV7592 <abhinavchandran77@gmail.com>
2026-01-02 17:02:11 +0530

    Updated

---
+++ service_booking/Models/Bookings.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace service_booking.Models
+{
+    public class Bookings
+    {
+        [Required]
+        public int user_id { get; set; }
+
+        [Required]
+        public int vehicle_id { get; set; }
+
+        [Required]
+        public int service_type_id { get; set; }
+
+        [Required]
+        public int slot_id { get; set; }
+
+        [Required]
+        public string? booking_status { get; set; }
+
+        [Required]
+        public DateTime date { get; set; }
+    }
+}
