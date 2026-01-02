73c3d2314d73a6572fe1de1bb1c96560af76e24d
ABHINAV7592 <abhinavchandran77@gmail.com>
2026-01-02 16:59:19 +0530

    Updated

---
+++ service_booking/Models/TimeSlot.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace service_booking.Models
+{
+    public class TimeSlot
+    {
+        [Required]
+        public DateTime slot_date { get; set; }
+
+        [Required]
+        public TimeSpan start_time { get; set; }
+
+        [Required]
+        public TimeSpan end_time { get; set; }
+
+        [Required]
+        public int max_bookings { get; set; }
+    }
+}
