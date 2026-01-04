b0f5d888a642c20bbf51490a1a952bb2c1d5a289
ABHINAV7592 <abhinavchandran77@gmail.com>
2026-01-04 11:29:50 +0530

    Updated

---
+++ service_booking/Controllers/UserHome.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace service_booking.Controllers
+{
+    public class UserHome : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
